1c0d6a4dffa29220b4e76b1b732e893dadfb73fb
dubrost <dubrost@users.noreply.github.com>
2020-05-01 20:52:35 +0300

    Update dadata.py

--- dadata.py
+++ dadata.py
@@ -9,45 +9,28 @@ class DaData:
         if secret:
             self.secret = str(secret)
 
-    def suggest(self, query, code, count=10):
+    def ask(self, uri, data):
         headers = {
-            "Authorization": self.token,
-            "Content-Type": "application/json",
-            "Accept": "application/json"
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': self.token
         }
+        return requests.post(uri, data=json.dumps(data), headers=headers)
+
+    def suggest(self, query, code, count=10):
         data = {
             'query': query,
             'count': count
         }
-        res = requests.post(
-            'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/' + str(code),
-            data=json.dumps(data),
-            headers=headers
-        )
-        return res.json()
+        return self.ask('https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/' + str(code), data).json()
 
     def findById(self, query, code):
-        headers = {
-            "Authorization": self.token,
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        }
         data = {
             "query": query
         }
-        res = requests.post(
-            'https://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/' + str(code),
-            data=json.dumps(data),
-            headers=headers
-        )
-        return res.json()
+        return self.ask('https://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/' + str(code), data).json()
 
     def geolocate(self, lat, lon, count=10, rad=100, lang='ru'):
-        headers = {
-            "Authorization": self.token,
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        }
         data = {
             'lon': str(lon),
             'lat': str(lat),
@@ -55,12 +38,14 @@ class DaData:
             'radius_meters': str(rad),
             'language': str(lang)
         }
-        res = requests.post(
-            'https://suggestions.dadata.ru/suggestions/api/4_1/rs/geolocate/address',
-            data=json.dumps(data),
-            headers=headers
-        )
-        return res.json()
+        return self.ask('https://suggestions.dadata.ru/suggestions/api/4_1/rs/geolocate/address', data).json()
+
+    def findAffiliated(self, query, count=10):
+        data = {
+            'query': str(query),
+            'count': str(count)
+        }
+        return self.ask('https://suggestions.dadata.ru/suggestions/api/4_1/rs/findAffiliated/party', data).json()
 
     def iplocate(self, query):
         headers = {
@@ -70,23 +55,6 @@ class DaData:
         res = requests.get(
             'https://suggestions.dadata.ru/suggestions/api/4_1/rs/iplocate/address?ip=' + str(query),
             headers=headers
-        )
-        return res.json()
-
-    def findAffiliated(self, query, count=10):
-        headers = {
-            "Authorization": self.token,
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        }
-        data = {
-            'query': str(query),
-            'count': str(count)
-        }
-        res = requests.post(
-            'https://suggestions.dadata.ru/suggestions/api/4_1/rs/findAffiliated/party',
-            headers=headers,
-            data=json.dumps(data)
         )
         return res.json()
 
